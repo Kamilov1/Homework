0fc0a23d8d3758418e1068686ec344b51f09df03
Kamilov1 <itprofessiya1@gmail.com>
2022-07-28 19:57:15 +0300

    Second problem ready

---
+++ Examples2/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+Console.Write("a = ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("b = ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("c = ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+string result;
+if(numberA >= numberB)
+{
+    result = "max = numberA";
+}
+else 
+{
+    result = "max = numberB";
+}
+if(numberA >= numberC)
+{
+    result = "max = numberA";
+}
+else
+{
+    result = "max = numberC";
+}
+Console.WriteLine(result);
